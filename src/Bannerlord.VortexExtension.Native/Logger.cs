99957cb0cb5409bae237a40863ece26315390c4d
Aragas <personal@aragas.org>
2022-09-24 13:16:25 +0300

    fix

--- src/Bannerlord.VortexExtension.Native/Logger.cs
+++ src/Bannerlord.VortexExtension.Native/Logger.cs
@@ -7,8 +7,10 @@ namespace Bannerlord.VortexExtension.Native
     {
         public static void Log(string message)
         {
+#if DEBUG
             var path = "D:\\Git\\Bannerlord.VortexExtension\\src\\Bannerlord.VortexExtension.Native\\bin\\Release\\net7.0\\win-x64\\native\\test.log";
             File.AppendAllText(path, $"{message}{Environment.NewLine}");
+#endif
         }
     }
 }
