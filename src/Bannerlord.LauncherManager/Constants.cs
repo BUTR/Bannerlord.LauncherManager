c5c5435eafff0f9197ec936504bdfaf856df6917
Vitalii Mikhailov <personal@aragas.org>
2023-04-12 15:34:20 +0300

    Fix

--- src/Bannerlord.LauncherManager/Constants.cs
+++ src/Bannerlord.LauncherManager/Constants.cs
@@ -8,7 +8,7 @@ public static class Constants
     public static string XboxConfiguration = "Gaming.Desktop.x64_Shipping_Client";
 
     public static string BannerlordExecutable = "Bannerlord.exe";
-    public static string BLSEExecutable = "Bannerlord.BLSE.exe";
+    public static string BLSEExecutable = "Bannerlord.BLSE.Standalone.exe";
 
     public static string SubModuleName = "SubModule.xml";
     public static string ModulesFolder = "Modules";
