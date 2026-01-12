068e83ac516999e338af8bf45b2b6de067f17460
Vitalii Mikhailov <personal@aragas.org>
2026-01-12 16:15:05 +0200

    Fix

--- src/Bannerlord.LauncherManager.Native.TypeScript/src/index.ts
+++ src/Bannerlord.LauncherManager.Native.TypeScript/src/index.ts
@@ -1,11 +1,10 @@
-import * as types from './types';
+import * as types from "./types";
 
-export * from './Common';
-export * from './BannerlordModuleManager';
-export * from './FetchBannerlordVersion';
-export * from './LauncherManager';
-export * from './Utils';
+export * from "./Common";
+export * from "./BannerlordModuleManager";
+export * from "./FetchBannerlordVersion";
+export * from "./LauncherManager";
+export * from "./Logger";
+export * from "./Utils";
 
-export {
-    types
-}
+export { types };
